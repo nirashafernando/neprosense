4327d8b890427af74576a83109d64ca8b4cf635e
sithijatharuka41-hub <sithijatharuka41@gmail.com>
2026-03-03 22:55:11 +0530

    Clean project - removed venv, db, unnecessary files

---
+++ Lifestyle_Frontend/Frontend/build/static/js/488.0a6842ba.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklifestyleprediction=self.webpackChunklifestyleprediction||[]).push([[488],{2488:(e,i,s)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.0a6842ba.chunk.js.map
